2e8fde83d02a1dd0ab938cc4f0b9a588d4b070b4
miunmnn <esteban.principe@utec.edu.pe>
2021-07-26 17:08:24 -0500

    a

--- examen/src/test/java/examen/test/MainTest.java
+++ examen/src/test/java/examen/test/MainTest.java
@@ -1,13 +1,2 @@
 package examen.test;
 
-import mainprogram.Main;
-import org.testng.Assert;
-import org.testng.annotations.Test;
-
-public class MainTest {
-    @Test
-    void test(){
-        Main.main(new String[]{});
-        Assert.assertTrue(true);
-    }
-}
